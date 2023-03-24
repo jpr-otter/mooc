4cd8b70d61caf3b5e578ecbc31e50cbe0fad9e07
jpr-otter <96299330+jpr-otter@users.noreply.github.com>
2023-03-25 00:00:12 +0100

    Update part03_26.java

--- part03_26.java
+++ part03_26.java
@@ -27,3 +27,30 @@ public class LineByLine {
 
 // classic! i didnt read properly. the script should stop when there is no input instead i thought it should stop if the input is "halted" simply because
 // is was the last word in the example outputs for the script... maybe this is why it s not accepted.
+
+/*
+
+i got it i got it... ez fix.
+
+
+Scanner scanner = new Scanner(System.in);
+        String input = ("");
+
+        while (true){
+
+            input = scanner.nextLine();
+            if (input.equals("")){
+                break;
+            }
+            String[] alreadySplit = input.split(" ");
+
+
+
+            for (int i = 0; i < alreadySplit.length ; i++) {
+                System.out.println(alreadySplit[i]);
+
+            }
+        }
+        
+        
+*/
