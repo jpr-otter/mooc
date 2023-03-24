3d603cc9ed70df1fcceb27206a9db14f10e0f1b8
jpr-otter <96299330+jpr-otter@users.noreply.github.com>
2023-03-25 00:40:22 +0100

    Create part03_29.java

---
+++ part03_29.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class LastWords {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            String input = scanner.nextLine();
+
+            if (input.length() == 0) {
+                break;
+            }
+
+            String[] words = input.split(" ");
+            System.out.println(words[words.length-1]);
+        }
+
+
+    }
+}
