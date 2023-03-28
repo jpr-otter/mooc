c2c352b3cac32620de7212f999843daa28608675
jpr-otter <96299330+jpr-otter@users.noreply.github.com>
2023-03-29 01:09:52 +0200

    Create Main.java

---
+++ part04_07/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Product mango = new Product("Mango",2, 13);
+        Product strawberry = new Product("Strawberry",10,20);
+
+        mango.printProduct();
+        strawberry.printProduct();
+        // This is an empty main method for testing the Product class
+        // Try:
+
+        // Product product = new Product("Banana", 1.1, 13);
+        // product.printProduct();
+    }
+}
