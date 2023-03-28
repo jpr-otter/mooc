75ebccb785b8a2537d93e445413b474cb5db3d65
jpr-otter <96299330+jpr-otter@users.noreply.github.com>
2023-03-28 23:59:39 +0200

    Create Main.java

---
+++ part04_05/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        // This is just an empty main method you can use to test
+        // the Whistle class. Try for example:
+
+        Whistle duckWhistle = new Whistle ("quaack");
+        Whistle roosterWhistle = new Whistle("gogock");
+
+        duckWhistle.sound();
+        duckWhistle.sound();
+        roosterWhistle.sound();
+        duckWhistle.sound();
+        duckWhistle.sound();
+
+//        Whistle duckWhistle = new Whistle("Kvaak");
+//        Whistle roosterWhistle = new Whistle("Peef");
+//
+//        duckWhistle.sound();
+//        roosterWhistle.sound();
+//        duckWhistle.sound();
+    }
+}
