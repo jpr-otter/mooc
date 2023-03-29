273be9e7e0ad41a72d2f6ed2c4ccb91f58518091
jpr-otter <96299330+jpr-otter@users.noreply.github.com>
2023-03-29 21:59:13 +0200

    Create Song.java

---
+++ part04_01/Song.java
@@ -0,0 +1,17 @@
+public class Song {
+    private int length;
+    private String name;
+
+    public Song(String name, int length){
+        this.name = name;
+        this.length = length;
+    }
+
+    public String name(){
+        return this.name;
+    }
+
+    public int length(){
+        return this.length;
+    }
+}
