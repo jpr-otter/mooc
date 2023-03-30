9722e11f6d44d4c7c90e3023220ae71512d6ec98
jpr-otter <96299330+jpr-otter@users.noreply.github.com>
2023-03-30 21:41:25 +0200

    Create Main.java

---
+++ part04_13/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Agent bond = new Agent ("Jannis", "Blond");
+
+        bond.toString();
+        System.out.println(bond);
+
+        Agent doubleOhSix = new Agent ("Sean","Bean");
+
+        System.out.println("TO STRING BEGINNING");
+        doubleOhSix.toString();
+        System.out.println("TO STRING END");
+        System.out.println(doubleOhSix);
+
+    }
+
+
+
+}
