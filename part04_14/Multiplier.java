2a4f6de96fe4e1b3e6e460dab5cd645e198118ed
jpr-otter <96299330+jpr-otter@users.noreply.github.com>
2023-03-31 00:47:05 +0200

    Create Multiplier.java

---
+++ part04_14/Multiplier.java
@@ -0,0 +1,12 @@
+public class Multiplier {
+
+    private int number;
+
+    public Multiplier(int number){
+        this.number = number;
+    }
+
+    public int multiplyBy(int number){
+        return this.number * number;
+    }
+}
