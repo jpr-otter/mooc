3b183c2456071f302e7538a8924989dab997bfa0
jpr-otter <96299330+jpr-otter@users.noreply.github.com>
2023-04-21 15:44:58 +0200

    Create Program.java

---
+++ part05_02/Program.java
@@ -0,0 +1,18 @@
+
+public class Program {
+
+    public static void main(String[] args) {
+        // This is simply an empty main method where you are
+        // free to experiment with your Book class.
+        // An example that you can use for testing:
+
+
+//        Book b = new Book(NO POTTER);
+//        System.out.println(b);
+
+        Book infiniteJest = new Book("David Foster Wallace","Infinite Jest", 1079);
+        System.out.println(infiniteJest.getAuthor());
+        System.out.println(infiniteJest);
+
+    }
+}
